3190d79798bfaa4377262fe16574bea7762484e5
Zachary Voase <z@zacharyvoase.com>
2012-11-14 12:15:29 +0000

    Initial commit.

---
+++ setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+
+
+setup(name='curly',
+      version='0.0.1',
+      description="Use the full power of the curl command line easily from Python.",
+      author='Zack Voase',
+      author_email='z@zacharyvoase.com',
+      url='https://github.com/zacharyvoase/curly',
+      py_modules=['curly'],
+      license='Unlicense')
