06f6ea549f1be75074b3247615c92f40a33be9f2
Zachary Voase <z@zacharyvoase.com>
2012-11-18 18:37:25 +0000

    Bumped to v0.0.3.

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup
 
 
 setup(name='curly',
-      version='0.0.2',
+      version='0.0.3',
       description="Use the full power of the curl command line easily from Python.",
       author='Zack Voase',
       author_email='z@zacharyvoase.com',
